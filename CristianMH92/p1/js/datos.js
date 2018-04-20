8574ca94c63b6941eb3b7646e14debf724d112fe
CristianMH92 <cristian92mar@gmail.com>
2018-04-20 14:55:58 -0500

    Changes

---
+++ CristianMH92/p1/js/datos.js
@@ -0,0 +1,22 @@
+(function DATOS(document){
+    'use strict';
+   
+   const URL = 'json/datos.json';
+  
+   fetch(URL)
+   .then(response=>response.json())
+    .then(response=>datos(response))
+    .catch('Error');
+    
+    const datos = (response) => {
+        for(const resp in response.data)
+            {
+                
+                let nombre = response.data[resp].nombre;
+                let mascota = response.data[resp].mascota;
+                
+                document.getElementById("contenidoDatos").innerHTML += `<tr><td>${nombre}</td><td>${mascota}</td></tr>`;
+            }
+    }
+
+})(document);
