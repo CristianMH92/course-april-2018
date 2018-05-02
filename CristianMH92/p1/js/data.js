9d322e1957c43e32f25a2676889c5ea484063e28
CristianMH92 <cristian92mar@gmail.com>
2018-05-02 15:02:30 -0500

    add my changes

---
+++ CristianMH92/p1/js/data.js
@@ -0,0 +1,22 @@
+(function DATOS(document){
+    'use strict';
+   
+   const URL = '../json/data.json';
+  
+   fetch(URL)
+   .then(response=>response.json())
+    .then(response=>information(response))
+    .catch('Error');
+    
+    const information = (response) => {
+        for(const resp in response.data)
+            {
+                
+                let name = response.data[resp].name;
+                let color = response.data[resp].color;
+                
+                document.getElementById("datacontent").innerHTML += `<tr><td>${name}</td><td>${color}</td></tr>`;
+            }
+    }
+
+})(document);
